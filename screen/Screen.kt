fcace5582e5e2301accd97672d6bccafad43f690
Damien Dervishi <DervishiD01@gmail.com>
2018-12-09 15:41:04 +0100

    constructing button system

--- screen/Screen.kt
+++ screen/Screen.kt
@@ -17,22 +17,64 @@ public class Screen : JPanel(){
             result.setBounds(0, 0, FRAMEX, FRAMEY);
             result.setLayout(null);
             
-            var startButton : JButton = initStartButton();
-            startButton.setBounds(HALF_FRAMEX - START_BUTTON_WIDTH, FRAMEY * 3 / 4 - START_BUTTON_HEIGHT,
-            					  START_BUTTON_WIDTH, START_BUTTON_HEIGHT);
-            result.add(startButton);
+            result.add(startButton());
+            result.add(exitButton());
+            result.add(optionsButton());
+            result.add(statsButton());
             
             return result;
         }
         
         @JvmStatic
-        private fun initStartButton() : JButton{
+        private fun startButton() : JButton{
             var result : JButton = JButton();
-            result.setFont(START_BUTTON_FONT);
+            result.setFont(DEFAULT_BUTTON_FONT);
             result.setBackground(WHITE);
             result.setText("Start");
+            result.addActionListener{println("TODO : WRITE STARTBUTTON ACTIONLISTENER");};
+            result.setBounds(START_BUTTON_X, START_BUTTON_Y, START_BUTTON_WIDTH, START_BUTTON_HEIGHT);
+            return result;
+        }
+        
+        @JvmStatic
+        private fun exitButton() : JButton{
+            var result : JButton = JButton();
+            result.setFont(DEFAULT_BUTTON_FONT);
+            result.setBackground(WHITE);
+            result.setText("X");
+            result.addActionListener{println("TODO : WRITE EXITBUTTON ACTIONLISTENER");};
+            result.setBounds(EXIT_BUTTON_X, EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH, EXIT_BUTTON_HEIGHT);
+            return result;
+        }
+        
+        @JvmStatic
+        private fun optionsButton() : JButton{
+            var result : JButton = JButton();
+            result.setFont(DEFAULT_BUTTON_FONT);
+            result.setBackground(WHITE);
+            result.setText("Options");
+            result.addActionListener{println("TODO : WRITE OPTIONSBUTTON ACTIONLISTENER");};
+            result.setBounds(OPTIONS_BUTTON_X, OPTIONS_BUTTON_Y, OPTIONS_BUTTON_WIDTH, OPTIONS_BUTTON_HEIGHT);
+            return result;
+        }
+        
+        @JvmStatic
+        private fun statsButton() : JButton{
+            var result : JButton = JButton();
+            result.setFont(DEFAULT_BUTTON_FONT);
+            result.setBackground(WHITE);
+            result.setText("Statistics");
+            result.addActionListener{println("TODO : WRITE STATSBUTTON ACTIONLISTENER");};
+            result.setBounds(STATS_BUTTON_X, STATS_BUTTON_Y, STATS_BUTTON_WIDTH, STATS_BUTTON_HEIGHT);
             return result;
         }
         
     }
 }
+
+
+
+
+
+
+
--- screen/ScreenConstants.kt
+++ screen/ScreenConstants.kt
@@ -2,8 +2,34 @@ package screen
 
 import java.awt.Font
 import java.awt.Font.PLAIN
+import main.HALF_FRAMEX
+import main.FRAMEX
+import main.FRAMEY
 
 public const val START_BUTTON_WIDTH : Int = 200;
-public const val START_BUTTON_HEIGHT : Int = 100;
-public val START_BUTTON_FONT : Font = Font("Arial", PLAIN, 16);
+public const val START_BUTTON_HEIGHT : Int = 60;
+public const val START_BUTTON_X : Int = HALF_FRAMEX - START_BUTTON_WIDTH / 2;
+public const val START_BUTTON_Y : Int = FRAMEY * 2 / 3 - START_BUTTON_HEIGHT / 2;
+
+public const val EXIT_BUTTON_WIDTH : Int = 60;
+public const val EXIT_BUTTON_HEIGHT : Int = 60;
+public const val EXIT_BUTTON_X : Int = FRAMEX - EXIT_BUTTON_WIDTH;
+public const val EXIT_BUTTON_Y : Int = 0;
+
+public const val OPTIONS_BUTTON_WIDTH : Int = 200;
+public const val OPTIONS_BUTTON_HEIGHT : Int = 60;
+public const val OPTIONS_BUTTON_X : Int = HALF_FRAMEX - OPTIONS_BUTTON_WIDTH / 2;
+public const val OPTIONS_BUTTON_Y : Int = START_BUTTON_Y + START_BUTTON_HEIGHT;
+
+public const val STATS_BUTTON_WIDTH : Int = 200;
+public const val STATS_BUTTON_HEIGHT : Int = 60;
+public const val STATS_BUTTON_X : Int = HALF_FRAMEX - STATS_BUTTON_WIDTH / 2;
+public const val STATS_BUTTON_Y : Int = OPTIONS_BUTTON_Y + OPTIONS_BUTTON_HEIGHT;
+
+public val DEFAULT_BUTTON_FONT : Font = Font("Arial", PLAIN, 16);
+
+
+
+
+
 
